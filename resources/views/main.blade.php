c4f39161d9ec203eacb0816f693f728bb6121a91
ima-yusuke <yusuke1125aadxyz@gmail.com>
2025-01-27 21:54:15 +0900

    add : template.blade.phpの追加

---
+++ resources/views/main.blade.php
@@ -0,0 +1,18 @@
+<x-template title="ete" css="app.css">
+    <div class="flex">
+        <section class="flex flex-col items-center justify-between bg-black h-[100vh] px-6 py-[5%]">
+            <P class="text-pink-400 text-5xl">●</P>
+            <nav class="text-white">
+                <ul class="flex flex-col gap-6">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+        </section>
+
+        <section>
+
+        </section>
+    </div>
+</x-template>
